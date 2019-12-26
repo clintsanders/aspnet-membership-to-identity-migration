83adf7718c207007bb56c3c7f5040109b281c994
clintsanders <clint.sanders@gmail.com>
2019-12-26 08:56:54 -0600

    Initial commit.

---
+++ Data/Migrations/20191226052421_identity-user-properties.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace membership_migration.Data.Migrations
+{
+    public partial class identityuserproperties : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Comment",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreateDate",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "FailedPasswordAnswerAttemptCount",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "FailedPasswordAnswerAttemptWindowStart",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "FailedPasswordAttemptCount",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "FailedPasswordAttemptWindowStart",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsApproved",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastActivityDate",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastLockoutDate",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastLoginDate",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastPasswordChangedDate",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "LegacyPasswordHash",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Comment",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "CreateDate",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "FailedPasswordAnswerAttemptCount",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "FailedPasswordAnswerAttemptWindowStart",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "FailedPasswordAttemptCount",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "FailedPasswordAttemptWindowStart",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "IsApproved",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastActivityDate",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastLockoutDate",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastLoginDate",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastPasswordChangedDate",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LegacyPasswordHash",
+                table: "AspNetUsers");
+        }
+    }
+}
---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace membership_migration.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+		public bool IsApproved { get; set; }
+		public DateTime LastActivityDate { get; set; }
+		public string LegacyPasswordHash { get; set; }
+		public DateTime CreateDate { get; set; }
+		public DateTime LastLoginDate { get; set; }
+		public DateTime LastPasswordChangedDate { get; set; }
+		public DateTime LastLockoutDate { get; set; }
+		public int FailedPasswordAttemptCount { get; set; }
+		public DateTime FailedPasswordAttemptWindowStart { get; set; }
+		public int FailedPasswordAnswerAttemptCount { get; set; }
+		public DateTime FailedPasswordAnswerAttemptWindowStart { get; set; }
+		public string Comment { get; set; }
+
+		public ApplicationUser()
+		{
+
+		}
+	}
+}
